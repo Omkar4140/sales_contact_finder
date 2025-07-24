cac3cbe00e374925d067452e07a1a213ef32d161
Omkar Lokhande <95611418+Omkar4140@users.noreply.github.com>
2025-07-24 15:47:40 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python
+
 import sys
 from sales_contact_finder.crew import SalesContactFinderCrew
 # This main file is intended to be a way for your to run your # crew locally, so refrain from adding necessary logic into this file. # Replace with inputs you want to test with, it will automatically # interpolate any tasks and agents information
